aafb225a13261e57582d9b18514fefc91ffffba5
Patrick Knabe <p.knabe@wut.de>
2021-02-02 12:57:50 +0100

    functionality added

--- limit-monitor.js
+++ limit-monitor.js
@@ -2,78 +2,104 @@ module.exports = RED => {
 	RED.nodes.registerType( 'limit-monitor', function( config ) {
 		RED.nodes.createNode( this, config );
 
-		const limit = parseFloat( config.limit );
-console.log( 'Grenzwert:', limit );
-		let prevVal;
+		const limit = parseFloat( config.limit ) || 0;
+		const delay = ( parseFloat( config.delay ) || 0 ) * 1000;
+		const hysteresis = parseFloat( config.hysteresis ) || 0;
+
+		// 1: no alarm
+		// 2: delaying
+		// 3: alarm
+		let state;
+
 		let timeout;
 
-		let timestamp;
-
-		const getPayload = value => config.output == 1 ? !value ? false : true : !value ? 0 : 1;
-
-		const setStatus = ( value, flash ) => {
-			if( timestamp === 0 ) {
-				this.status( { fill: 'grey', shape: 'dot', text: 'inactive' } );
-			} else if( flash ) {
-				this.status( { fill: 'green', shape: 'dot', text: `active: ${ getPayload( value ) } / ${ getPayload( !value ) }` } );
-				setTimeout( setStatus, 250, value, false );
-			} else {
-				this.status( { fill: 'grey', shape: 'dot', text: `active: ${ getPayload( value ) } / ${ getPayload( !value ) }` } );
-			}
-		};
-
-		const start = () => {
-			if( timestamp === 0 ) {
-				timestamp = new Date().getTime();
-				update( true );
-			}
-		};
-
-		const stop = () => {
-			if( timestamp !== 0 ) {
-				timestamp = 0;
-				update( false );
-			}
-		};
-
-		const update = value => {
-			msg = {
+		const send = () => {
+			this.send( {
 				name: config.name,
 				topic: config.topic,
-				payload: getPayload( value ),
+				payload: state === 3,
 				timestamp: new Date().getTime()
-			};
+			} );
+		};
 
-			setStatus( value, true );
-			this.send( [ msg, { ...msg, payload: getPayload( !value ) } ] );
+		const setStatus = ( tmpState ) => {
+			state = tmpState;
 
-			if( timestamp !== 0 ) {
-				while( timestamp <= msg.timestamp ) timestamp += config.period * 500;
-				timeout = setTimeout( update, timestamp - msg.timestamp, !value );
-			} else {
-				clearTimeout( timeout );
+			switch( state ) {
+
+				case 1:
+					this.status( { fill: 'green', shape: 'dot', text: 'no alarm' } );
+					break;
+
+				case 2:
+					this.status( { fill: 'yellow', shape: 'dot', text: 'delaying' } );
+					break;
+
+				case 3:
+					this.status( { fill: 'red', shape: 'dot', text: 'alarm' } );
+					break;
+
 			}
 		};
 
+		setStatus( 1 );
+
 		this.on( 'input', msg => {
-			const value = parseFloat( msg.payload );
+			if( parseFloat( msg.payload ) !== Number.NaN ) {
+				if( config.kind === '1' ) { // lower limit
+					switch( true ) {
 
-			switch( config.kind ) {
+						case state === 1 && msg.payload <= limit:
+							setStatus( 2 );
+							timeout = setTimeout( () => {
+								setStatus( 3 );
+								send();
+							}, delay );
 
-				case '1': // lower limit
-					break;
+							break;
 
-				case '2': // upper limit
-					break;
+						case state === 2 && msg.payload > limit:
+							setStatus( 1 );
+							clearTimeout( timeout );
 
-			}
+							break;
 
-			if( ( prevVal === undefined || prevVal < limit ) && msg.payload >= limit ) {
-				console.log( 'Grenze überschritten' );
-			} else {
-				console.log( 'keine Grenzwertverletzung' );
+						case state === 3 && msg.payload > limit - hysteresis:
+							setStatus( 1 );
+							send();
+
+							break;
+
+					}
+				} else { // upper limit
+					switch( true ) {
+
+						case state === 1 && msg.payload >= limit:
+							setStatus( 2 );
+							timeout = setTimeout( () => {
+								setStatus( 3 );
+								send();
+							}, delay );
+
+							break;
+
+						case state === 2 && msg.payload < limit:
+							setStatus( 1 );
+							clearTimeout( timeout );
+
+							break;
+
+						case state === 3 && msg.payload < limit - hysteresis:
+							setStatus( 1 );
+							send();
+
+							break;
+
+					}
+				}
 			}
 		} );
+
 		this.on( 'close', () => clearTimeout( timeout ) );
 	} );
 };
